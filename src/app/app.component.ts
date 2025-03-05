b9f1cc02065f437880cd1fd7a484a32ea4f342f4
ADarko22 <a.elessar22@gmail.com>
2025-03-05 14:26:52 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-resume-website';
+  title = 'Angelo Buono\'s Home';
+
+  constructor() {
+    console.log('✅ AppComponent Initialized');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{ path: 'intro', component: '' }
+    //{ path: 'work-experience', component: '' }
+    //{ path: 'education', component: '' }
+    //{ path: 'hobbies', component: '' }
+    //{ path: 'contacts', component: '' }
+];
